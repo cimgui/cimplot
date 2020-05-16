eaa49abf485bca95db30764c84ca544f62a25533
Victor Bombi <sonoroster@gmail.com>
2020-05-16 13:51:22 +0200

    pull and generate implot 0.2WIP

--- generator/output/typedefs_dict.lua
+++ generator/output/typedefs_dict.lua
@@ -1,8 +1,9 @@
 local defs = {}
-defs["ImAxisFlags"] = "int"
-defs["ImMarker"] = "int"
+defs["ImPlotAxisFlags"] = "int"
 defs["ImPlotCol"] = "int"
 defs["ImPlotFlags"] = "int"
+defs["ImPlotLimits"] = "struct ImPlotLimits"
+defs["ImPlotMarker"] = "int"
 defs["ImPlotRange"] = "struct ImPlotRange"
 defs["ImPlotStyle"] = "struct ImPlotStyle"
 defs["ImPlotStyleVar"] = "int"
