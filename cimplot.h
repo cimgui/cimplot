f7fbb3e3646fc4f15665d01c564a9273e6d3e56c
Victor Bombi <sonoroster@gmail.com>
2021-03-09 10:58:51 +0100

    generation

--- cimplot.h
+++ cimplot.h
@@ -591,13 +591,15 @@ CIMGUI_API void ImPlot_SetImGuiContext(ImGuiContext* ctx);
 CIMGUI_API void ImPlot_ShowDemoWindow(bool* p_open);
 
 
+typedef void *(*ImPlotPoint_getter)(void* data, int idx, ImPlotPoint *point);
+
 //ImPlotPoint getters manually wrapped
-CIMGUI_API void ImPlot_PlotLineG(const char* label_id,ImPlotPoint*(*getter)(void* data,int idx),void* data,int count,int offset);
-CIMGUI_API  void ImPlot_PlotScatterG(const char* label_id, ImPlotPoint* (*getter)(void* data, int idx), void* data, int count, int offset);
-CIMGUI_API void ImPlot_PlotShadedG(const char* label_id, ImPlotPoint* (*getter1)(void* data, int idx), void* data1, ImPlotPoint* (*getter2)(void* data, int idx), void* data2, int count, int offset);
-CIMGUI_API void ImPlot_PlotBarsG(const char* label_id, ImPlotPoint* (*getter)(void* data, int idx), void* data, int count, double width, int offset);
-CIMGUI_API void ImPlot_PlotBarsHG(const char* label_id, ImPlotPoint* (*getter)(void* data, int idx), void* data, int count, double height,  int offset);
-CIMGUI_API void ImPlot_PlotDigitalG(const char* label_id, ImPlotPoint* (*getter)(void* data, int idx), void* data, int count, int offset);
+CIMGUI_API void ImPlot_PlotLineG(const char* label_id,ImPlotPoint_getter getter,void* data,int count,int offset);
+CIMGUI_API  void ImPlot_PlotScatterG(const char* label_id, ImPlotPoint_getter getter, void* data, int count, int offset);
+CIMGUI_API void ImPlot_PlotShadedG(const char* label_id, ImPlotPoint_getter getter1, void* data1, ImPlotPoint_getter getter2, void* data2, int count, int offset);
+CIMGUI_API void ImPlot_PlotBarsG(const char* label_id, ImPlotPoint_getter getter, void* data, int count, double width, int offset);
+CIMGUI_API void ImPlot_PlotBarsHG(const char* label_id, ImPlotPoint_getter getter, void* data, int count, double height,  int offset);
+CIMGUI_API void ImPlot_PlotDigitalG(const char* label_id, ImPlotPoint_getter getter, void* data, int count, int offset);
 
 #endif //CIMGUIPLOT_INCLUDED
 
