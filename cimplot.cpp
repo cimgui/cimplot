3f50d8ea5a455ad65108368af705e469b3e0024b
Victor Bombi <sonoroster@gmail.com>
2021-08-22 14:22:55 +0200

    generate

--- cimplot.cpp
+++ cimplot.cpp
@@ -2835,39 +2835,39 @@ ImPlotPoint Wrapper2(void* data, int idx)
 	return pp;
 }
 
-CIMGUI_API void ImPlot_PlotLineG(const char* label_id, ImPlotPoint_getter getter,void* data,int count,int offset)
+CIMGUI_API void ImPlot_PlotLineG(const char* label_id, ImPlotPoint_getter getter,void* data,int count)
 {
     getter_funcX = getter;
-	ImPlot::PlotLineG(label_id,Wrapper,data,count,offset);
+	ImPlot::PlotLineG(label_id,Wrapper,data,count);
 }
 
-CIMGUI_API  void ImPlot_PlotScatterG(const char* label_id, ImPlotPoint_getter getter, void* data, int count, int offset)
+CIMGUI_API  void ImPlot_PlotScatterG(const char* label_id, ImPlotPoint_getter getter, void* data, int count)
 {
 	getter_funcX = getter;
-	ImPlot::PlotScatterG(label_id, Wrapper, data, count, offset);
+	ImPlot::PlotScatterG(label_id, Wrapper, data, count);
 }
 
-CIMGUI_API void ImPlot_PlotShadedG(const char* label_id, ImPlotPoint_getter getter1, void* data1, ImPlotPoint_getter getter2, void* data2, int count, int offset)
+CIMGUI_API void ImPlot_PlotShadedG(const char* label_id, ImPlotPoint_getter getter1, void* data1, ImPlotPoint_getter getter2, void* data2, int count)
 {
 	getter_funcX = getter1;
 	getter_funcX2 = getter2;
-	ImPlot::PlotShadedG(label_id, Wrapper, data1, Wrapper2, data2, count, offset);
+	ImPlot::PlotShadedG(label_id, Wrapper, data1, Wrapper2, data2, count);
 }
 
-CIMGUI_API void ImPlot_PlotBarsG(const char* label_id, ImPlotPoint_getter getter, void* data, int count, double width, int offset)
+CIMGUI_API void ImPlot_PlotBarsG(const char* label_id, ImPlotPoint_getter getter, void* data, int count, double width)
 {
 	getter_funcX = getter;
-	ImPlot::PlotBarsG(label_id, Wrapper, data, count, width, offset);
+	ImPlot::PlotBarsG(label_id, Wrapper, data, count, width);
 }
 
-CIMGUI_API void ImPlot_PlotBarsHG(const char* label_id, ImPlotPoint_getter getter, void* data, int count, double height,  int offset)
+CIMGUI_API void ImPlot_PlotBarsHG(const char* label_id, ImPlotPoint_getter getter, void* data, int count, double height)
 {
 	getter_funcX = getter;
-	ImPlot::PlotBarsHG(label_id, Wrapper, data, count, height, offset);
+	ImPlot::PlotBarsHG(label_id, Wrapper, data, count, height);
 }
 
-CIMGUI_API void ImPlot_PlotDigitalG(const char* label_id, ImPlotPoint_getter getter, void* data, int count, int offset)
+CIMGUI_API void ImPlot_PlotDigitalG(const char* label_id, ImPlotPoint_getter getter, void* data, int count)
 {
 	getter_funcX = getter;
-	ImPlot::PlotDigitalG(label_id, Wrapper, data, count, offset);
+	ImPlot::PlotDigitalG(label_id, Wrapper, data, count);
 }
