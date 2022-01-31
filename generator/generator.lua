9014992292651ee28652a96a61694048a273dc97
sonoro1234 <sonoroster@gmail.com>
2022-01-31 18:06:32 +0100

    generator: changes for new generation

--- generator/generator.lua
+++ generator/generator.lua
@@ -9,10 +9,10 @@ local COMPILER = script_args[1]
 local INTERNAL_GENERATION = (script_args[2] and script_args[2]:match("internal")) and true or false
 local CPRE,CTEST
 if COMPILER == "gcc" or COMPILER == "clang" then
-    CPRE = COMPILER..[[ -E -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS -DIMGUI_API="" -DIMGUI_IMPL_API="" ]]
+    CPRE = COMPILER..[[ -E -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS -DIMPLOT_DISABLE_OBSOLETE_FUNCTIONS -DIMGUI_API="" -DIMGUI_IMPL_API="" ]]
     CTEST = COMPILER.." --version"
 elseif COMPILER == "cl" then
-    CPRE = COMPILER..[[ /E /DIMGUI_DISABLE_OBSOLETE_FUNCTIONS /DIMGUI_API="" /DIMGUI_IMPL_API="" ]]
+    CPRE = COMPILER..[[ /E /DIMGUI_DISABLE_OBSOLETE_FUNCTIONS /DIMPLOT_DISABLE_OBSOLETE_FUNCTIONS /DIMGUI_API="" /DIMGUI_IMPL_API="" ]]
     CTEST = COMPILER
 else
     print("Working without compiler ")
@@ -94,52 +94,6 @@ local func_implementation = cpp2ffi.func_implementation
 
 ----------custom ImVector templates (from cimgui, but removed need to separate pool as ImGuiStorage already declared in cimgui)
 local table_do_sorted = cpp2ffi.table_do_sorted
-local function generate_templates(code,templates)
-    -- already declared in imgui
-    -- table.insert(code,"\n"..[[typedef struct ImVector{int Size;int Capacity;void* Data;} ImVector;]].."\n")
-	--skip ImVector already in cimgui
-	-- generated ImVector are added for avoiding duplicates
-	local ImVectorSkipped = {
-		float = true,
-		ImU32 = true,
-		ImGuiColorMod = true,
-		ImGuiStyleMod = true,
-	}
-	table_do_sorted(templates , function (ttype, v)
-		if ttype == "ImVector" then
-			table_do_sorted(v, function(te,newte)
-                if not ImVectorSkipped[te] then -- these are already declared in imgui
-				    table.insert(code,"typedef struct ImVector_"..newte.." {int Size;int Capacity;"..te.."* Data;} ImVector_"..newte..";\n")
-					ImVectorSkipped[te] = true
-                end
-			end)
-		elseif ttype == "ImPool" then
-			--declare ImGuiStorage
-			table_do_sorted(v, function(te, newte)
-				if not ImVectorSkipped[te] then
-					table.insert(code,"typedef struct ImVector_"..newte.." {int Size;int Capacity;"..te.."* Data;} ImVector_"..newte..";\n")
-					ImVectorSkipped[te] = true
-				end
-				table.insert(code,"typedef struct ImPool_"..newte.." {ImVector_"..te.." Buf;ImGuiStorage Map;ImPoolIdx FreeIdx;} ImPool_"..newte..";\n")
-			end)
-		elseif ttype == "ImChunkStream" then
-			table_do_sorted(v, function(te,newte)
-				if not ImVectorSkipped[te] then
-					table.insert(code,"typedef struct ImVector_"..newte.." {int Size;int Capacity;"..te.."* Data;} ImVector_"..newte..";\n")
-					ImVectorSkipped[te] = true
-				end
-				table.insert(code,"typedef struct ImChunkStream_"..newte.." {ImVector_"..te.." Buf;} ImChunkStream_"..newte..";\n")
-			end)
-		elseif ttype == "ImSpan" then
-			table_do_sorted(v, function(te,newte)
-				table.insert(code,"typedef struct ImSpan_"..newte.." {"..te.."* Data;" ..te.."* DataEnd;} ImSpan_"..newte..";\n")
-			end)
-		else
-			print("generate_templates ttype not done",ttype)
-			error"generate templates"
-		end
-	end)
-end
 
 --generate cimgui.cpp cimgui.h 
 local function cimgui_generation(parser,name)
@@ -151,12 +105,9 @@ local function cimgui_generation(parser,name)
 	cpp2ffi.prtable(parser.templates)
 	cpp2ffi.prtable(parser.typenames)
 
-	local outtab = {}
-    generate_templates(outtab, parser.templates)
+	local tdt = parser:generate_templates()
 
-
-	--local cstructsstr = outpre..outpost 
-	local cstructsstr = outpre.."\n"..table.concat(outtab,"")..outpost 
+	local cstructsstr = outpre..tdt..outpost 
 
     hstrfile = hstrfile:gsub([[#include "imgui_structs%.h"]],cstructsstr)
     local cfuncsstr = func_header_generate(parser)
@@ -205,6 +156,8 @@ local function parseImGuiHeader(header,names)
 	parser.manuals = cimgui_manuals
 	parser.skipped = cimgui_skipped
 	parser.UDTs = {"ImVec2","ImVec4","ImColor","ImRect","ImPlotPoint","ImPlotLimits","ImPlotTime"}
+	--parser.gentemplatetypedef = gentemplatetypedef
+	parser.cimgui_inherited =  dofile([[../../cimgui/generator/output/structs_and_enums.lua]])
 	--this list will expand all templated functions
 	parser.ftemplate_list = {}
 	parser.ftemplate_list.T = {"float", "double", "ImS8", "ImU8", "ImS16", "ImU16", "ImS32", "ImU32", "ImS64", "ImU64"}
@@ -235,7 +188,7 @@ parser1:do_parse()
 save_data("./output/overloads.txt",parser1.overloadstxt)
 cimgui_generation(parser1,modulename)
 save_data("./output/definitions.lua",serializeTableF(parser1.defsT))
-local structs_and_enums_table = parser1:gen_structs_and_enums_table()
+local structs_and_enums_table = parser1.structs_and_enums_table
 save_data("./output/structs_and_enums.lua",serializeTableF(structs_and_enums_table))
 save_data("./output/typedefs_dict.lua",serializeTableF(parser1.typedefs_dict))
 
